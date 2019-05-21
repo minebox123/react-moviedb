81dd3c014a094b31d9c70d9058356f1a42016aa9
minebox123 <solodeev.09@mail.com>
2019-05-21 12:20:25 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -19,25 +19,21 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Switch>
-          <div className="App">
-            <Header />
-
-            <div className="main-container">
-              <Route exact path="/" component={MainContainer} />
-              <Route path="/tv" component={TV} />
-              <Route path="/people" component={PeopleContainer} />
-              <Route path="/login" component={Login} />
-              <Route path="/movieInfo/:id" component={MovieInfo} />
-              <Route path="/showInfo/:id" component={ShowInfo} />
-              <Route path="/actorInfo/:id" component={ActorInfo} />
-              <Route path="/results/:userInput" component={SearchResults} />
-              <Route exact path="/guestPage" component={Guest} />
-            </div>
-
-            <Footer />
-          </div>
-        </Switch>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route path="/tv" exact component={TV} />
+            <Route path="/people" exact component={PeopleContainer} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/movieInfo/:id" exact component={MovieInfo} />
+            <Route path="/showInfo/:id" exact component={ShowInfo} />
+            <Route path="/actorInfo/:id" exact component={ActorInfo} />
+            <Route path="/results/:userInput" exact component={SearchResults} />
+            <Route exact path="/guestPage" component={Guest} />
+          </Switch>
+          <Route path="/" exact component={MainContainer} />
+          <Footer />
+        </div>
       </Router>
     );
   }
